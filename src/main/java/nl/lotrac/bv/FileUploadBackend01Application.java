af339af09f963a6a44b91747f8b472efe6bf2cb3
=Rob Franke <robwfranke@gmail.com>
2021-06-24 10:06:25 +0200

    opstart

---
+++ src/main/java/nl/lotrac/bv/FileUploadBackend01Application.java
@@ -0,0 +1,13 @@
+package nl.lotrac.bv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadBackend01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadBackend01Application.class, args);
+	}
+
+}
